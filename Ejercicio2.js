9709d54eeefe096721008673af142f93003d30b8
gabrielguerrero <galgues9@gmail.com>
2022-04-02 17:17:25 -0300

    arreglo de desafio

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -4,9 +4,10 @@ let numero1 = parseInt(prompt("Ingrese valor de su primer producto"));
 let numero2 = parseInt(prompt("Ingrese valor de su segundo producto"));
 let numero3 = parseInt(prompt("Ingrese valor de su tercer producto"));
 
-function productos (numero1, numero2, numero3){
-    return numero1 + numero2 + numero3;
+function productos (a, b, c){
+    let suma = numero1 + numero2 + numero3;
+    return suma;
 }
 
-let resultado = sumar(numero1, numero2, numero3);
+let resultado = productos(numero1, numero2, numero3);
 console.log("El total de productos es", resultado);
