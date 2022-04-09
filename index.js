bc59ade85a64930b99d1cc83e3dcc46241f917bd
gabrielguerrero <galgues9@gmail.com>
2022-04-09 19:19:05 -0300

    Modificaciones

--- index.js
+++
@@ -1,8 +0,0 @@
-//Ejercicio con While
-
-let capitalArgentina = prompt("Ingresa la capital de Argentina");
-
-    while(capitalArgentina != "Buenos Aires"){
-        alert("Dato erroneo");
-        capitalArgentina = prompt("Ingrese nuevamente la capital de Argentina")
-    }
